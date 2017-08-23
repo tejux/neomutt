0944a527f14ce360ee1b69b0e9d5a0b00b5eec9a
Pietro Cerutti <gahr@gahr.ch>
2017-08-23 13:05:48 +0000

    Retain signatures even if fcc_clear is set
    Issue #727

--- send.c
+++ send.c
@@ -2038,7 +2038,7 @@ int ci_send_message(int flags, struct Header *msg, char *tempfile,
     struct Body *save_sig = NULL;
     struct Body *save_parts = NULL;
 
-    if (WithCrypto && (msg->security & (ENCRYPT | SIGN)) && FccClear)
+    if (WithCrypto && (msg->security & ENCRYPT) && FccClear)
       msg->content = clear_content;
 
     /* check to see if the user wants copies of all attachments */
